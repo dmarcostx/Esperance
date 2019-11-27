c90360a2d1da3b63980b2e09853286c868b344d2
Marcos Teixeira <marcos.teixeira13@fatec.sp.gov.br>
2019-11-26 23:02:32 -0300

    Added cart get router

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const loginRouter = require('./routes/login');
+const cartRouter = require('./routes/cart');
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
+app.use('/cart', cartRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
