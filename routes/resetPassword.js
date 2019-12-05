b5d80a5088f5eae49093d12cc7665bcf29cd09d7
Marcos Teixeira <marcos.teixeira13@fatec.sp.gov.br>
2019-12-05 11:40:39 -0300

    Managing reset password

--- routes/resetPassword.js
+++ routes/resetPassword.js
@@ -1,9 +1,12 @@
 var express = require('express')
 var router = express.Router()
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('resetPassword')
+  res.render('resetPassword', {posted: false})
+})
+
+router.post('/', function (req, res, next) {
+  res.render('resetPassword', {posted: true})
 })
 
 module.exports = router
